2c66cfad98f5702f64d81ea536a4e66779fd73f5
A138634 <dsmith9@agl.com.au>
2019-08-09 13:23:16 +1000

    Update json_test.cs

--- Test_Json_Decoder/json_test.cs
+++ Test_Json_Decoder/json_test.cs
@@ -13,28 +13,16 @@ namespace Test_Json_Decoder
     {
         static void Main(string[] args)
         {
-            object content;
-
             // read JSON file into an Object
             try
             {
-                 content = Json_Decoder.Json.Load(args[0]);
+                 var content = Json_Decoder.Json.Load(args[0]);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
 
-            // parse JSON string into an Object
-            try
-            {
-                string json = File.ReadAllText(args[0]);
-                content = Json_Decoder.Json.Parse(json);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
         }
     }
 }
